5aea1e5389f44734ad7235f65c213428eb8c9cdd
KALI PRASAD <kancharlaguruprasad@gmail.com>
2022-10-15 22:17:31 +0530

    update constants

--- src/main/java/com/kotakcherry/stocksearch/constants/Constants.java
+++ src/main/java/com/kotakcherry/stocksearch/constants/Constants.java
@@ -3,7 +3,9 @@ package com.kotakcherry.stocksearch.constants;
 public class Constants {
 
     public static final String FILE_UPLOAD_SUCCESS = "File uploaded successfully";
-    public static final String FILE_UPLOAD_FAILED = "Could not upload the file:";
-    public static final String INVALID_FILE_FORMAT = "Please upload an excel file!";
+    public static final String STOCK_ADDED = "Stock added successfully";
+    public static final String STOCK_FOUND = "Stock fetched successfully";
+    public static final String STOCKS_FOUND = "Stocks fetched successfully for given keywords";
+    public static final String STOCKS_DELETED = "All stocks are successfully deleted";
 
 }
