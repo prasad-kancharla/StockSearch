adc95ad034763ed6ca6124d04074aedb02088d4f
KALI PRASAD <kancharlaguruprasad@gmail.com>
2022-10-15 22:16:33 +0530

    update stock repository

--- src/main/java/com/kotakcherry/stocksearch/repository/StockRepository.java
+++ src/main/java/com/kotakcherry/stocksearch/repository/StockRepository.java
@@ -6,11 +6,16 @@ import org.springframework.data.elasticsearch.repository.ElasticsearchRepository
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface StockRepository extends ElasticsearchRepository<Stock, Long> {
 
     List<Stock> findAll();
+
+    long count();
+
+    Optional<Stock> findById(Long id);
 
     List<Stock> search(QueryBuilder query);
 }
