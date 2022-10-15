47e34020b57048cf49d7ba74c0de49adc64ed140
KALI PRASAD <kancharlaguruprasad@gmail.com>
2022-10-15 20:04:13 +0530

    add default tests

---
+++ src/test/java/com/kotakcherry/stocksearch/StocksearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kotakcherry.stocksearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StocksearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
