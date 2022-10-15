56c81f11c4bf0c81679ab7769a09c97eee8ec530
KALI PRASAD <kancharlaguruprasad@gmail.com>
2022-10-15 20:02:56 +0530

    add constants

---
+++ src/main/java/com/kotakcherry/stocksearch/constants/Constants.java
@@ -0,0 +1,9 @@
+package com.kotakcherry.stocksearch.constants;
+
+public class Constants {
+
+    public static final String FILE_UPLOAD_SUCCESS = "File uploaded successfully";
+    public static final String FILE_UPLOAD_FAILED = "Could not upload the file:";
+    public static final String INVALID_FILE_FORMAT = "Please upload an excel file!";
+
+}
