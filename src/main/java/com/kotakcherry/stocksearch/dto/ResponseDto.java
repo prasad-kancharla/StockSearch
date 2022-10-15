ed9c0aa4785a5624b4eca2ccc17969eeb9056014
KALI PRASAD <kancharlaguruprasad@gmail.com>
2022-10-15 22:17:09 +0530

    add response dto

---
+++ src/main/java/com/kotakcherry/stocksearch/dto/ResponseDto.java
@@ -0,0 +1,12 @@
+package com.kotakcherry.stocksearch.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+
+@Data
+@AllArgsConstructor
+public class ResponseDto {
+    private HttpStatus status;
+    private String message;
+}
