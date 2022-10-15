ac304ef797fd3170334df10d2a50ab88e78d62fd
KALI PRASAD <kancharlaguruprasad@gmail.com>
2022-10-15 20:03:39 +0530

    add stock application

---
+++ src/main/java/com/kotakcherry/stocksearch/StockSearchApplication.java
@@ -0,0 +1,13 @@
+package com.kotakcherry.stocksearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockSearchApplication.class, args);
+	}
+
+}
